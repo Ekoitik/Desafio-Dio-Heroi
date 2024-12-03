0b99a07f4e448bb8d08d9aa04b4fcc83644774ac
edsonkk <koiticamaro@gmail.com>
2024-12-02 21:02:03 -0300

    Code ready

---
+++ desafio.js
@@ -0,0 +1,28 @@
+let nome = "Edson";
+let xp =  10001;
+let nivel = "";
+
+if(xp < 1000){
+    nivel = "Ferro";
+}
+else if(xp < 2001){
+    nivel = "Bronze";
+}
+else if(xp < 5001){
+    nivel = "Prata";
+}else if (xp < 7001){
+    nivel = "Ouro";
+}
+else if(xp < 8001){
+    nivel = "Platina";
+}
+else if(xp < 9001){
+    nivel = "Ascendente";
+}
+else if(xp < 10001){
+    nivel = "Imortal";
+}
+else{
+    nivel = "Radiante";
+}
+console.log("O Herói de nome **"+ nome + "** está no nível de **" + nivel+"**");
